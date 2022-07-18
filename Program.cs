b607fbf1f98539b63ce1162d79af11d159e76dc3
Anna <107642838+Kashitsina@users.noreply.github.com>
2022-07-18 20:12:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿void Zadacha41()
+{
+//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь
+Console.WriteLine($"Cколько чисел больше 0");
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int count = 0 ;
+
+for (int i = 0; i < m; i++)
+{   
+    Console.WriteLine();
+    Console.WriteLine($"Введите {i} число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if(number >0 ) count++;
+}
+Console.WriteLine("Чисел больше нуля:" + count);
+}
+Zadacha41();
